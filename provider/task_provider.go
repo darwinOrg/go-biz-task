00d2ebbac744bf51b9c71e7cd209f408fb9314e6
chenbiao <149027310@qq.com>
2024-04-10 10:23:38 +0800

    refactor

--- provider/task_provider.go
+++ provider/task_provider.go
@@ -120,6 +120,8 @@ func ReInitTimeoutProcessingTasks(ctx *dgctx.DgContext, tc *daog.TransContext, t
 func convertTaskVo(task *task_dal.BizTask) *task_model.CommonTaskVo {
 	return &task_model.CommonTaskVo{
 		Id:               task.Id,
+		TaskType:         int(task.Type),
+		Channel:          int(task.Channel),
 		Content:          task.Content.StringNilAsEmpty(),
 		ScheduledStartAt: task.ScheduledStartAt.String(),
 		ScheduledEndAt:   task.ScheduledEndAt.String(),
